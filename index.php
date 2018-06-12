020bf1701bc5d540a5b8dd498f56a6003621c276
Brendon Dimal <brendondimal@gmail.com>
2018-06-12 16:05:20 +0530

    fixing issues

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
+<?php
 class Animal{
 	
 	var $animal_name = '';
 
+	function setName($new_name){
+
+		$this->animal_name;
+	}
+
+	function getName(){
+
+		return $this->animal_name;
+	}
+
 	
 
 }
+
+?>
